3456571256bd0a0c762f77ce6524b10b1347110b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-06-11 00:59:13 -0400

    Left eye should go left

--- binocular_camera.gd
+++ binocular_camera.gd
@@ -3,12 +3,17 @@ extends Node3D
 @onready var left = $LeftEyeContainer/LeftEye/Camera3D
 @onready var right = $RightEyeContainer/RightEye/Camera3D
 
-# need to allow adjustment of FOV and IPD ingame via navigatable pause scene
-# (black void like with quest)
+@export_range(40, 80) var ipd_mm : int = 62
+@export_range(90, 120) var FOV : int = 110
+
+# need to allow adjustment of FOV and IPD ingame via
+# navigatable pause scene (black void like with quest)
 
 func _ready() -> void:
-	left.h_offset = 0.031
-	right.h_offset = -0.031
 	
-	left.fov = 110
-	right.fov = 110
+	# divided by 2000 to get half of IPD and to convert to meters
+	left.h_offset = ipd_mm / -2000.0
+	right.h_offset = ipd_mm / 2000.0
+	
+	left.fov = FOV
+	right.fov = FOV
